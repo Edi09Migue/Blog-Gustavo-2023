4a354b6d27f4fd8b9e40c438ef281d3e100c29e8
Jaime Santana <sistemas@santana.ec>
2021-06-15 17:40:01 -0500

    Fix, handle server error en permisos

--- frontend/src/views/apps/staff/permission/permissions-list/usePermissionsList.js
+++ frontend/src/views/apps/staff/permission/permissions-list/usePermissionsList.js
@@ -1,77 +1,80 @@
-import { ref, watch, computed } from '@vue/composition-api'
-import store from '@/store'
+import { ref, watch, computed } from "@vue/composition-api";
+import store from "@/store";
 
 // Notification
-import { useToast } from 'vue-toastification/composition'
-import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
-import { useUtils as useI18nUtils } from '@core/libs/i18n'
+import { useToast } from "vue-toastification/composition";
+import ToastificationContent from "@core/components/toastification/ToastificationContent.vue";
+import { useUtils as useI18nUtils } from "@core/libs/i18n";
 
-export default function userPermissionsList(){
-    const { t } = useI18nUtils()
-    const toast = useToast()
+export default function userPermissionsList() {
+    const { t } = useI18nUtils();
+    const toast = useToast();
 
-    const refItemsListTable = ref(null)
-     // Table Handlers
+    const refItemsListTable = ref(null);
+    // Table Handlers
     const tableColumns = [
-        { key: 'name', sortable: true, label: t('Permission') },
-        { key: 'guard_name', sortable: true, label: t('Guard Name') },
-        { key: 'group_key', sortable: true, label: t('Group Key') },
-        { key: 'actions' , label: t('Actions')},
-    ]
-    const perPage = ref(10)
-    const totalItems = ref(0)
-    const currentPage = ref(1)
-    const perPageOptions = [10, 25, 50, 100]
-    const searchQuery = ref('')
-    const sortBy = ref('id')
-    const isSortDirDesc = ref(true)
-    const groupFilter = ref(null)
+        { key: "name", sortable: true, label: t("Permission") },
+        { key: "guard_name", sortable: true, label: t("Guard Name") },
+        { key: "group_key", sortable: true, label: t("Group Key") },
+        { key: "actions", label: t("Actions") }
+    ];
+    const perPage = ref(10);
+    const totalItems = ref(0);
+    const currentPage = ref(1);
+    const perPageOptions = [10, 25, 50, 100];
+    const searchQuery = ref("");
+    const sortBy = ref("id");
+    const isSortDirDesc = ref(true);
+    const groupFilter = ref(null);
 
     const dataMeta = computed(() => {
-        const localItemsCount = refItemsListTable.value ? refItemsListTable.value.localItems.length : 0
+        const localItemsCount = refItemsListTable.value
+            ? refItemsListTable.value.localItems.length
+            : 0;
         return {
-          from: perPage.value * (currentPage.value - 1) + (localItemsCount ? 1 : 0),
-          to: perPage.value * (currentPage.value - 1) + localItemsCount,
-          of: totalItems.value,
-        }
-      })
-    
-      const refetchData = () => {
-        refItemsListTable.value.refresh()
-      }
-    
-      watch([currentPage, perPage, searchQuery, groupFilter], () => {
-        refetchData()
-      })
+            from:
+                perPage.value * (currentPage.value - 1) +
+                (localItemsCount ? 1 : 0),
+            to: perPage.value * (currentPage.value - 1) + localItemsCount,
+            of: totalItems.value
+        };
+    });
 
-      const fetchPermissions = (ctx, callback) => {
+    const refetchData = () => {
+        refItemsListTable.value.refresh();
+    };
+
+    watch([currentPage, perPage, searchQuery, groupFilter], () => {
+        refetchData();
+    });
+
+    const fetchPermissions = (ctx, callback) => {
         store
-          .dispatch('app-permission/fetchPermissions', {
-            q: searchQuery.value,
-            perPage: perPage.value,
-            page: currentPage.value,
-            sortBy: sortBy.value,
-            sortDesc: isSortDirDesc.value,
-            group: groupFilter.value,
-          })
-          .then(response => {
-            const { items, total } = response.data
-    
-            callback(items)
-            totalItems.value = total
-          })
-          .catch(() => {
-            toast({
-              component: ToastificationContent,
-              props: {
-                title: 'Error fetching permissions list',
-                icon: 'AlertTriangleIcon',
-                variant: 'danger',
-              },
+            .dispatch("app-permission/fetchPermissions", {
+                q: searchQuery.value,
+                perPage: perPage.value,
+                page: currentPage.value,
+                sortBy: sortBy.value,
+                sortDesc: isSortDirDesc.value,
+                group: groupFilter.value
             })
-          })
-      }
+            .then(response => {
+                const { items, total } = response.data;
 
+                callback(items);
+                totalItems.value = total;
+            })
+            .catch(() => {
+                toast({
+                    component: ToastificationContent,
+                    props: {
+                        title: "Error fetching permissions list",
+                        icon: "AlertTriangleIcon",
+                        variant: "danger"
+                    }
+                });
+            });
+    };
 
     return {
         fetchPermissions,
@@ -90,5 +93,5 @@ export default function userPermissionsList(){
 
         //Extra filters
         groupFilter
-    }
+    };
 }
