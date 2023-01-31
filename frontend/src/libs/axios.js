463832a7f322bc85591b17d5a692ac6b78978413
Edisson <edi09migue@gmail.com>
2023-01-30 21:44:07 -0500

    Axios

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -6,9 +6,8 @@ import axios from "axios";
 const axiosIns = axios.create({
     // You can add your headers here
     // ================================
-    baseURL: "http://controlelectoral.local/"
-    // baseURL: "https://ceec.ec/"
-    //baseURL: "https://api.todo63.com/"
+    // baseURL: "http://controlelectoral.local/"
+    baseURL: "https://ceec.ec/"
     // timeout: 1000,
     // headers: {'X-Custom-Header': 'foobar'}
 });
